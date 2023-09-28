0bf08fd0a2985e89d732523c26e0f0ad153dfdd9
Saglara <nohaevass@yandex.ru>
2023-09-28 21:57:03 +0300

    1

---
+++ sem1/ex/dz2/managment/commands/create_customer.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from dz2.models import Сustomer
+class Command(BaseCommand):
+    help = "Create user."
+    def handle(self, *args, **kwargs):
+        user = Сustomer(name='John', email='john@example.com',
+                        phone='+71231231212', adress = 'Электросталь, ул. Ленина, 23д. 5кв.', 
+                        date = 12/12/2000)
+        user.save()
+        self.stdout.write(f'{user}')
